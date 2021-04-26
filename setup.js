6c8ed6a44ffa2b22ef72d2ebf05c5daae9bf16a4
alanclarke <alan@qubit.com>
2021-04-26 12:26:42 +0100

    Flesh out mock placement api

--- setup.js
+++ setup.js
@@ -1,5 +1,27 @@
+/* istanbul ignore file */
+const Promise = require('sync-p/extra')
+const poller = require('@qubit/poller')
+const getBrowserState = require('@qubit/jolt/lib/getBrowserState')
+const path = require('path')
+
 module.exports = function setup (overrides) {
+  const log = {
+    trace: jest.fn(),
+    info: jest.fn(),
+    error: jest.fn()
+  }
   const cleanups = []
+  const packageJson = getPkgJson()
+  const {
+    placementId,
+    trackingId,
+    namespace,
+    vertical,
+    domains = [window.location.hostname],
+    visitorId = 'visitorId',
+    isPreview = false
+  } = packageJson.meta || {}
+
   return {
     api: {
       elements: [],
@@ -7,10 +29,74 @@ module.exports = function setup (overrides) {
       onRemove: cleanup => cleanups.push(cleanup),
       onImpression: jest.fn(),
       onClickthrough: jest.fn(),
+      getBrowserState: jest.fn(() => Promise.resolve(getBrowserState())),
+      getVisitorState: jest.fn(),
+      log,
+      meta: {
+        cookieDomain: getCookieDomain(domains),
+        isPreview,
+        namespace,
+        placementId,
+        trackingId,
+        vertical,
+        visitorId
+      },
+      poll: createPoller(log),
+      uv: {
+        emit: jest.fn(),
+        events: [],
+        on: jest.fn(),
+        once: jest.fn(),
+        onceEventSent: jest.fn(),
+        onEventSent: jest.fn()
+      },
       ...overrides
     },
     teardown: () => {
       while (cleanups.length) cleanups.pop()()
     }
   }
+}
+
+function createPoller (logger) {
+  return function poll (targets, options) {
+    if (typeof options !== 'function') {
+      options = _.assign(
+        {
+          logger,
+          stopOnError: true
+        },
+        options
+      )
+    }
+    return poller(targets, options)
+  }
+}
+
+function getPkgJson () {
+  try {
+    return require(path.join(process.cwd(), 'package.json'))
+  } catch (err) {
+    return {}
+  }
+}
+
+function getCookieDomain (domains, currentHost) {
+  currentHost = currentHost || window.location.hostname
+
+  var match = domains.find(function (domain) {
+    var rDomain = regexify(domain)
+    return rDomain.test(currentHost)
+  })
+  return match
+}
+
+function regexify (domain) {
+  return new RegExp(
+    '(^|\\.)' + removeLeadingDot(domain).replace(/\./g, '\\.') + '$'
+  )
+}
+
+function removeLeadingDot (domain) {
+  return domain.replace(/^\./, '')
 }
